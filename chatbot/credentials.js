33f5d19a87706f176f9e6e0cf377e6bcfabc1202
Harshith u <44830597+Harshith-umesh@users.noreply.github.com>
2020-04-16 11:31:47 +0530

    Add files via upload

---
+++ chatbot/credentials.js
@@ -0,0 +1,18 @@
+
+    var credentialsAccessToken ="5ae7adf062fa4b5692087da997d2e3a5";
+
+    //var bot name is used for the firebase database
+    var credentialsBotName = "Emoji Bot";
+
+
+    var credentialsBaseUrl = "https://api.api.ai/v1/";
+
+    // Initialize Firebase
+    var credentialsConfig = {
+        apiKey: "AIzaSyBV5PiUUxluc5ISR2hcItUH0T7HEH6_ENo",
+        authDomain: "chatinterfacedb.firebaseapp.com",
+        databaseURL: "https://chatinterfacedb.firebaseio.com",
+        projectId: "chatinterfacedb",
+        storageBucket: "chatinterfacedb.appspot.com",
+        messagingSenderId: "922251038710"
+    };
